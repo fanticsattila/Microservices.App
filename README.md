d31fb68ce7a485b54db7b4a313b95ba30f84b53c
Fántics Attila <fantics.attila@windowslive.com>
2020-10-11 17:50:35 +0200

    Catalog API   - Asp.Net Core Web API   - Repository Pattern   - MongoDb   - Swagger   - Docker-compose

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Microservices.App
 https://www.udemy.com/course/microservices-architecture-and-implementation-on-dotnet/ tanfolyam által készített alkalmazás
+
+MongoDb telepítése a Projekthez
+	NuGet Package Manager segítségével telepítsük fel a következő csomagokat:
+		- MongoDB.Driver
+		- MongoDB.Bson
+
+MongoDb telepítése Dockerben:
+	- docker pull mongo
+
+MongoDb indítása Dockerben:
+	- docker run -d -p 27017:27017 --name aspnetrun-mongo mongo
+
+MongoDb elérése Dockeren belül:
+	- docker exec -it aspnetrun-mongo /bin/bash
+	- mongo
+		- adatbázisok lekérése:
+			show dbs
+		- adatbázis létrehozása:
+			use CatalogDb
+		- Collection létrehozása a CatalogDb-ben:
+			db.createCollection('Products')
+
+Swagger telepítése a Projekthez
+	NuGet Package Manager segítségével telepítsük fel a következő csomagot:
+		- Swashbuckle.AspNetCore
+
+Docker-compose futtatása (Generálása: jobb klikk a projekt-en, és menüből kiválasztani)
+	docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
+
+
+	
--- src/Catalog/Catalog.API/Startup.cs
+++ src/Catalog/Catalog.API/Startup.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Catalog.API.Data;
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Repositories;
+using Catalog.API.Repositories.Interfaces;
+using Catalog.API.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Catalog.API
 {
@@ -25,6 +25,22 @@ namespace Catalog.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.Configure<CatalogDatabaseSettings>(Configuration.GetSection(nameof(CatalogDatabaseSettings)));
+            services.AddSingleton<ICatalogDatabaseSettings>(sp => 
+                sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
+
+            services.AddTransient<ICatalogContext, CatalogContext>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Catalog API",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +58,12 @@ namespace Catalog.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c=>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API V1");
             });
         }
     }
